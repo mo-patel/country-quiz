4073a0c7d5915f3ea71757ee32b49affb8ffd28d
m0 <muhammed97patel@hotmail.com>
2022-03-25 18:13:37 +0000

    Added sample data

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css';
 import { QuizCardComponent } from './components/QuizCardComponent';
+import data from "./data/sample-data.json"
 
 function App() {
   return (
     <div className="App">
 
-      <QuizCardComponent />
+      <QuizCardComponent data={data} />
     </div>
   );
 }
--- src/components/QuizCardComponent.tsx
+++ src/components/QuizCardComponent.tsx
@@ -1,18 +1,31 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import "../styles/quizCardStyles.css"
+import { Question } from "../types/Question";
+import { OptionComponent } from "./OptionComponent";
 
 interface quizCardProps {
-
+    data: Question[]
 }
 
-export const QuizCardComponent: FC<quizCardProps> = () => {
+export const QuizCardComponent: FC<quizCardProps> = ({data}) => {
+    const [questions, setQuestions] = useState(data)
+    const [currentQuestion, setCurrentQuestion] = useState(1)
     return (
         <div className="quizCardParent">
             <h2>Country quiz</h2>
             <div className="quizCard">
                 <img className="adventureImg" src="/undraw_adventure.svg" alt="adventure" />
                 <div className="quiz">
-                    text flow
+                    {
+                        questions.map(item => {
+                            return (item.id === currentQuestion) ? 
+                            <div>
+                                <p>{item.description}</p>
+                                <OptionComponent /> 
+                            </div>
+                            : false
+                        })
+                    }
                 </div>
             </div>
         </div>
